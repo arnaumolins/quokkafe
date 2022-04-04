67579672ea6386de5dfec64e2bac5731c3e22542
arnau <amolins27@gmail.com>
2022-04-04 16:32:09 +0200

    Setup structured tools bars done.

---
+++ app/src/main/java/github/com/arnaumolins/quokkafe/UI/sign_in_fragment.java
@@ -0,0 +1,38 @@
+package github.com.arnaumolins.quokkafe.UI;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import github.com.arnaumolins.quokkafe.R;
+
+public class sign_in_fragment extends Fragment {
+
+    public sign_in_fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        getActivity().findViewById(R.id.bottom_navigation).setVisibility(View.GONE);
+        getActivity().findViewById(R.id.appbar_top).setVisibility(View.GONE);
+    }
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_sign_in_fragment, container, false);
+        return view;
+    }
+}
--- app/src/main/java/github/com/arnaumolins/quokkafe/UI/starting_page_fragment.java
+++ app/src/main/java/github/com/arnaumolins/quokkafe/UI/starting_page_fragment.java
@@ -2,11 +2,15 @@ package github.com.arnaumolins.quokkafe.UI;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 import github.com.arnaumolins.quokkafe.R;
 
@@ -18,14 +22,34 @@ public class starting_page_fragment extends Fragment {
     }
 
     @Override
+    public void onResume(){
+        super.onResume();
+        getActivity().findViewById(R.id.bottom_navigation).setVisibility(View.GONE);
+        getActivity().findViewById(R.id.appbar_top).setVisibility(View.GONE);
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+    Button signinButton;
+    Button loginButton;
+    ProgressBar progressBar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_starting_page_fragment, container, false);
+        View view = inflater.inflate(R.layout.fragment_starting_page_fragment, container, false);
+        signinButton = (Button) view.findViewById(R.id.sign_in_user_button);
+        signinButton.setOnClickListener( l -> {
+            Navigation.findNavController(getView()).navigate(R.id.action_starting_page_fragment_to_sign_in_fragment);
+        });
+        loginButton = (Button) view.findViewById(R.id.log_in_user_button);
+        loginButton.setOnClickListener( l -> {
+            Navigation.findNavController(getView()).navigate(R.id.action_starting_page_fragment_to_log_in_fragment);
+        });
+        return view;
     }
 }
