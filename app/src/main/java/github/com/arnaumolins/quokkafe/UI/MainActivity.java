0c09fe395bb662836166b50c8201f911000c52e7
arnau <amolins27@gmail.com>
2022-04-04 11:42:20 +0200

    Setup structured fragments.

---
+++ app/src/main/java/github/com/arnaumolins/quokkafe/UI/MainActivity.java
@@ -0,0 +1,62 @@
+package github.com.arnaumolins.quokkafe.UI;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+import github.com.arnaumolins.quokkafe.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNav;
+    NavController navController;
+    NavHostFragment navHostFragment;
+    Toolbar mToolbar;
+    AppBarConfiguration appBarConfiguration;
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // top bar
+        mToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
+
+        //nav controller
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
+        navController = navHostFragment.getNavController();
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, mToolbar, R.string.nav_drawe_open, R.string.nav_drawe_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationUI.setupWithNavController((NavigationView)findViewById(R.id.navigation_view), navController);
+
+        //bottom bar
+        bottomNav = findViewById(R.id.bottom_navigation);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
